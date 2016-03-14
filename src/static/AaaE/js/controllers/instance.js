c6007d211e20d95881595629221b088a1273646e
nino-c <nino.cocchiarella@gmail.com>
2016-03-14 14:58:58 -0700

    separation of seed transformations, asynch math-expression-parse types waituponing, seed editor, etc.

--- src/static/AaaE/js/controllers/instance.js
+++ src/static/AaaE/js/controllers/instance.js
@@ -16,10 +16,11 @@ angular
         $scope.seedTouched = false;
         $scope.readyToSave = false;
 
-        $scope.app = AppService.get({id:$route.current.params.app_id})
+        //$scope.app = AppService.get({id:$route.current.params.app_id})
         $scope.instance = InstanceService.get({id:$route.current.params.instance_id})
 
         $scope.instance.$promise.then(function() {
+            $scope.seedStructure = $scope.instance.game.seedStructure
             $scope.execute();
         })
 
@@ -28,106 +29,166 @@ angular
             $scope.readyToSave = true;
         }
 
+        $scope.transformSeed = function() {
+            $scope._seed = JSON.parse($scope.instance.seed)
+            $scope.seedStructure = JSON.parse($scope.instance.game.seedStructure)
+           
+            // add values into seedStructure to create
+            // object to pass into directive
+            
+            $scope.hasAsynchType = false;
+            $scope.asynchToWaitUpon = 0;
+            $scope._seed = _.mapObject($scope.seedStructure, function(val, key) {
+
+                if (val.type == 'math') {
+
+                    $scope.hasAsynchType = true;
+                    $scope.asynchToWaitUpon++;
+
+                    var _val = $scope._seed[key];
+                    if (typeof val.value != "object") {
+
+                        // assume that value is string
+                        val.value = {
+                            string: _val,
+                            javascript: '',
+                            latex: ''
+                        }
+
+                        $http({
+                            method: 'POST',
+                            url: "/symbolic_math/latex/"
+                            data: {
+                                expressionString: _val
+                            },
+                            headers: {
+                                'Content-Type': 'application/json'
+                            }
+                        }).then(function(response) {
+
+                            val.value.javascript = response.javascript;
+                            val.value.latex = '$'+response.latex+'$';
+
+                            $scope.asynchToWaitUpon--;
+
+                        }, function(response) {
+                            alert("Could not parse expression.")
+                          });
+
+                    }
+                } else {
+                    val.value = $scope._seed[key];
+                }
+
+
+
+                return val;
+            })
+            //console.log($scope._seed)
+            
+            // then convert to array for ng-repeat 
+            $scope.seedList = _.pairs($scope._seed);
+            //console.log($scope.seedList)
+        }
+
+        $scope.evaluateSource = function() {
+            // prepare code to eval
+            // first evaluate the seed to extract the variables inside the dict
+            var seedline = "var seed = " + $scope.instance.seed + ";"
+            eval(seedline)
+
+            // line-by-line for the system-generated part
+            var seedcodelines = [seedline]
+
+            // canvas declarations
+            seedcodelines.push( 'var canvas = $("#big-canvas");' )
+            seedcodelines.push( 'var Canvas = document.getElementById("big-canvas");' )
+
+            // control panels
+            //controlPanel = $("#floating-display-control");
+            //controlPanel.css({'display':'block'});
+
+            // canvas declarations
+            seedcodelines.push( 'canvas.css({\'display\':\'block\'});' )
+            seedcodelines.push( 'Canvas.width = $(window).width();' )
+            seedcodelines.push( 'Canvas.height = $(window).height()-50;' )
+            seedcodelines.push( 'console.log(Canvas);' )
+            seedcodelines.push( 'console.log(canvas);' )
+
+            // import seed attributes into local namespace
+            for (attr in seed) {
+
+                var line;
+                if (typeof seed[attr] == 'string') {
+
+                    // if color field, add colorpicker to form
+                    // if (seed[attr].toString().indexOf("rgba(") === 0) {
+                    //  if ($("#color_"+attr.toString())) {
+                    //      $("#color_"+attr.toString()).colorpicker();
+                    //  }
+                    // }
+
+                    line = "var " + attr + " = \""
+                        + seed[attr].toString() + "\";"
+                } else {
+                    line = "var " + attr + " = " + seed[attr].toString() + ";"
+                }
+                seedcodelines.push(line);
+
+            }
+
+            var source = seedcodelines.join("\n") + "\n"
+                + $scope.instance.sourcecode
+                + "\n try { start(); } catch(e) {}"
+
+            function updateElapsedTime() {
+                $scope.timeElapsed = ((new Date()).getTime() - $scope.appstart.getTime()) / 1000;
+            }
+
+            $scope.appstart = new Date();
+            timer = $interval(updateElapsedTime, 1000);
+
+            // execute seed code and game script
+            if ($scope.instance.game.scriptType == "text/paperscript") {
+
+                $scope.clearPaperCanvas();
+                
+                eval( seedcodelines.join("\n") );
+                $scope.loading = false;
+                $scope.gameFunction = new Function('Canvas', 'canvas', 
+                    'paper', 
+                    'with (paper) { ' + source 
+                        + '}')
+                $scope.gameFunction(Canvas, canvas, paper)
+                
+
+            } else {
+
+                $scope.clearCanvas();
+
+                console.log(seedcodelines.join("\n"));
+                eval( seedcodelines.join("\n") );
+                $scope.loading = false;
+                $scope.gameFunction = new Function('Canvas', 'canvas', source)
+                $scope.gameFunction(Canvas, canvas)
+
+            }
+        }
+
         $scope.execute = function() {
             if ($scope.instance.seed) {
 
-                $scope._seed = JSON.parse($scope.instance.seed)
-                console.log("_seed", $scope._seed)
-
-                var seedComponents = [];
-                for (var key in $scope._seed) {
-                    seedComponents.push({property: key, value: $scope._seed[key]})
-                }
-                $scope.seedComponents = seedComponents;
-                console.log('seedComponents', seedComponents)
-
-                // prepare code to eval
-
-                // first evaluate the seed to extract the variables inside the dict
-                var seedline = "var seed = " + $scope.instance.seed + ";"
-                eval(seedline)
-
-                // line-by-line for the system-generated part
-                var seedcodelines = [seedline]
-
-                // canvas declarations
-                seedcodelines.push( 'var canvas = $("#big-canvas");' )
-                seedcodelines.push( 'var Canvas = document.getElementById("big-canvas");' )
-
-                // control panels
-                //controlPanel = $("#floating-display-control");
-                //controlPanel.css({'display':'block'});
-
-                // canvas declarations
-                seedcodelines.push( 'canvas.css({\'display\':\'block\'});' )
-                seedcodelines.push( 'Canvas.width = $(window).width();' )
-                seedcodelines.push( 'Canvas.height = $(window).height()-50;' )
-                seedcodelines.push( 'console.log(Canvas);' )
-                seedcodelines.push( 'console.log(canvas);' )
-
-                // import seed attributes into local namespace
-                for (attr in seed) {
-
-                    var line;
-                    if (typeof seed[attr] == 'string') {
-
-                        // if color field, add colorpicker to form
-                        // if (seed[attr].toString().indexOf("rgba(") === 0) {
-                        //  if ($("#color_"+attr.toString())) {
-                        //      $("#color_"+attr.toString()).colorpicker();
-                        //  }
-                        // }
-
-                        line = "var " + attr + " = \""
-                            + seed[attr].toString() + "\";"
-                    } else {
-                        line = "var " + attr + " = " + seed[attr].toString() + ";"
-                    }
-                    seedcodelines.push(line);
-
-                }
-
-                var source = seedcodelines.join("\n") + "\n"
-                    + $scope.instance.sourcecode
-                    + "\n try { start(); } catch(e) {}"
-
-                function updateElapsedTime() {
-                    $scope.timeElapsed = ((new Date()).getTime() - $scope.appstart.getTime()) / 1000;
-                }
-
-                $scope.appstart = new Date();
-                timer = $interval(updateElapsedTime, 1000);
-
-                // execute seed code and game script
-                if ($scope.instance.game.scriptType == "text/paperscript") {
-
-                    $scope.clearPaperCanvas();
-                    
-                    eval( seedcodelines.join("\n") );
-                    $scope.loading = false;
-                    $scope.gameFunction = new Function('Canvas', 'canvas', 
-                        'paper', 
-                        'with (paper) { ' + source 
-                            + '}')
-                    $scope.gameFunction(Canvas, canvas, paper)
-                    
-
+                $scope.transformSeed();
+                
+                if (!$scope.hasAsynchType) {
+                    $scope.evaluateSource();
                 } else {
+                    $interval(function() {
 
-                    $scope.clearCanvas();
-
-                    console.log(seedcodelines.join("\n"));
-                    eval( seedcodelines.join("\n") );
-                    $scope.loading = false;
-                    $scope.gameFunction = new Function('Canvas', 'canvas', source)
-                    $scope.gameFunction(Canvas, canvas)
-
+                    }, 100)
                 }
 
-                  
-
-               
-
+            }
         }
     }
 
--- src/static/AaaE/js/directives/directives.js
+++ src/static/AaaE/js/directives/directives.js
@@ -22,15 +22,77 @@ angular.module('Exhibition')
             }
         }
     })
-    .component('seedDisplay', {
-        bindings: {
-            __seed: '='
-        },
-        controller: function() {
-            
-        },
-        templateUrl: 'views/seedDisplay.html'
+    .directive('seedField', function($compile) {
+        return {
+            restrict: 'E',
+            //require: 'ngModel',
+            scope: {
+                //ngModel: '@',
+                label: '=',
+                type: '=',
+                value: '='
+                //seed: '=ngModel'
+
+            },
+            //replace: true,
+            //require: 'ngModel',
+            //transclude: true,
+            templateUrl: 'seedField.html',
+            link: function(scope, elem, attrs, ctrl) {
+                console.log('------------', scope)
+                console.log(attrs)
+                console.log(attrs.struct)
+                console.log(ctrl)
+
+                //ctrl.$render = function() {
+                    // if (!$viewValue) {
+                    //     $viewValue = {
+                    //         type: seed[1].type,
+                    //         label: seed[0],
+                    //         value: seed[1].value
+                    //     }
+                    // }
+
+                    console.debug(attr.ngModel);
+                    console.debug($scope.$parent.$eval(attr.ngModel));
+
+                    // $scope.type =   ctrl.$viewValue.type;
+                    // $scope.label =  ctrl.$viewValue.label;
+                    // $scope.value =  ctrl.$viewValue.value;
+                    $scope.$apply();
+                //}
+
+                ctrl.seedChange = function() {
+
+                }
+            }
+        }
     })
+    .directive('seedEditor', function() {
+        return {
+            restrict: 'E',
+            require: 'ngModel',
+            templateUrl: 'seedEditor.html',
+            link: function(scope, elem, attrs, ngModel) {
+                //ngModel.$render = function() {
+                    scope.seedlist = _.map(ngModel.$viewValue, 
+                        function(item) {
+                            return {label: item[0], value: item[1].value}
+                        })
+                    console.log($scope.seedlist);
+                //}
+            }
+        }
+    })
+    // .component('seedDisplay', {
+    //     bindings: {
+    //         __seed: '='
+    //     },
+    //     controller: function() {
+            
+    //     },
+    //     templateUrl: 'views/seedDisplay.html'
+    // })
     // .directive('gameTimer', function() {
     //     return function(scope, element, attrs) {
             
